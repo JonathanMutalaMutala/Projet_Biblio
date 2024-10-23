6225cfebbb66b38ac7bd7a4b6dbad3a4308d9173
Mutala Mutala <jonamut12.live@gmail.com>
2024-10-22 20:01:24 -0400

    Add member element

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_Biblio.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModel/MemberDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projet_Biblio.ViewModel
+{
+    public class MemberDto
+    {
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+    }
+}
